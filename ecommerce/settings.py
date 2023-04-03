06e2a90b2d39e0742f73b2ffc7ea8c23907cadb9
shefin <developer.shefin@gmail.com>
2023-04-03 11:52:37 +0530

    deployed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -146,3 +146,5 @@ EMAIL_HOST_PASSWORD = 'xyz' # host email password required
 # otherwise you will get SMTPAuthenticationError at /contactus
 # this process is required because google blocks apps authentication by default
 EMAIL_RECEIVING_USER = ['to@gmail.com'] # email on which you will receive messages sent from website
+
+ALLOWED_HOSTS = ['*']
